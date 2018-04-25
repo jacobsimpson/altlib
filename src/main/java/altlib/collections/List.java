be7e520d5d02cb1eb1d869877fc763b9385c51f6
Jacob Simpson <jacob.simpson@gmail.com>
2018-04-25 00:17:13 -0700

    Added interface comments.

--- src/main/java/altlib/collections/List.java
+++ src/main/java/altlib/collections/List.java
@@ -3,9 +3,38 @@ package altlib.collections;
 import java.util.function.Function;
 
 public interface List<T> {
+    /**
+     * Returns the requested indexed element from the list.
+     * @param i requested element
+     * @return requested element
+     * @throws IndexOutOfBoundsException - if the requested element is out of range (i < 0 || i >= size())
+     */
     T at(int i);
+
+    /**
+     * Returns the number of elements in the list.
+     * @return number of elements in the list.
+     */
     int size();
+
+    /**
+     * Checks if there are no elements in the current list.
+     * @return returns the result of size() == 0
+     */
     boolean isEmpty();
-    List<T> add(T... items);
+
+    /**
+     * Executes the specified function for each element of the current list and generates a new list, with the same type as the current list, containing the results of the execution of the function.
+     * @param function mapping function to execute for each element of the list
+     * @param <R> type of the result of the mapping function
+     * @return a new list of the same type as the current list
+     */
     <R> List<R> map(Function<T, R> function);
+
+    /**
+     * Adds the specified items to the end of the list.
+     * @param items items to add
+     * @return the list itself
+     */
+    List<T> add(T... items);
 }
--- src/main/java/altlib/collections/ReadableList.java
+++ src/main/java/altlib/collections/ReadableList.java
@@ -3,9 +3,32 @@ package altlib.collections;
 import java.util.function.Function;
 
 public interface ReadableList<T> {
+    /**
+     * Returns the requested indexed element from the list.
+     * @param i requested element
+     * @return requested element
+     * @throws IndexOutOfBoundsException - if the requested element is out of range (i < 0 || i >= size())
+     */
     T at(int i);
+
+    /**
+     * Returns the number of elements in the list.
+     * @return number of elements in the list.
+     */
     int size();
+
+    /**
+     * Checks if there are no elements in the current list.
+     * @return returns the result of size() == 0
+     */
     boolean isEmpty();
+
+    /**
+     * Executes the specified function for each element of the current list and generates a new list, with the same type as the current list, containing the results of the execution of the function.
+     * @param function mapping function to execute for each element of the list
+     * @param <R> type of the result of the mapping function
+     * @return a new list of the same type as the current list
+     */
     <R> List<R> map(Function<T, R> function);
 }
 
